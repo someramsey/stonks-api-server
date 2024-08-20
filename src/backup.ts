d3c933e310357b8c6b1258108b449eb8bc81421e
Ramsey <someramseyy@gmail.com>
2024-08-20 14:15:03 +0200

    a

--- src/backup.ts
+++ src/backup.ts
@@ -3,10 +3,15 @@ import { setStocks, stocks } from "./stocks";
 const backupFile = Bun.file('backup.json');
 
 if(await backupFile.exists()) {
+    console.log("BACKUP EXISTS LOADING");
+
     backupFile.json().then((data) => {
+        console.log("LOADED BACKUP", data);
         if(data && data.length > 0) {
             setStocks(data);
         }
+    }).catch((err) => {
+        console.error("Error loading backup", err);
     });
 }
 
