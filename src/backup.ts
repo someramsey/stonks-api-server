62e8ba3b9a7c1f3836133b98cd9d108222d89e76
Ramsey <someramseyy@gmail.com>
2024-08-20 21:53:21 +0200

    stable again?

--- src/backup.ts
+++ src/backup.ts
@@ -1,22 +1,7 @@
 import { setStocks, stocks } from "./stocks";
 
-const backupFile = Bun.file('backup.json');
-
-if(await backupFile.exists()) {
-    console.log("BACKUP EXISTS LOADING");
-
-    backupFile.json().then((data) => {
-        console.log("LOADED BACKUP", data);
-        if(data && data.length > 0) {
-            setStocks(data);
-        }
-    }).catch((err) => {
-        console.error("Error loading backup", err);
-    });
-} else {
-    console.log("NO BACKUP FOUND");
-}
+//TODO: use airtable
 
 export async function backupStocks() {
-    Bun.write(backupFile, JSON.stringify(stocks));    
+    
 }
