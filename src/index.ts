a51002a48fca63875ff4d8b7bcc92d2f6e301d5a
Ramsey <someramseyy@gmail.com>
2024-08-20 20:43:27 +0200

    STABLE BEFORE NODE

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import { backupStocks } from "./backup";
 const server = serve({ fetch: router.fetch });
 console.log(`Server running at http://localhost:${server.port}`);
 
+
 setInterval(updateStocks, STOCK_UPDATE_INTERVAL);
 setInterval(backupStocks, STOCK_BACKUP_INTERVAL);
 console.log("Stocks update started");
